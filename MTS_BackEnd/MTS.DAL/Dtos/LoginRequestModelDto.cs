1ab117c8b67e16137048850e79aaa7c2b0e9e16b
chaulb <chouladila@gmail.com>
2025-06-28 18:25:09 +0700

    [BE01] Login
    [BE01] Login

--- MTS_BackEnd/MTS.DAL/Dtos/LoginRequestModelDto.cs
+++ MTS_BackEnd/MTS.DAL/Dtos/LoginRequestModelDto.cs
@@ -1,8 +1,13 @@
-﻿namespace MTS.DAL.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MTS.DAL.Dtos
 {
 	public class LoginRequestModelDto
 	{
+		[Required(ErrorMessage = "Username is required.")]
 		public string UserName { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Password is required.")]
 		public string Password { get; set; } = string.Empty;
 	}
 }
