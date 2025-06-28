5f7cd6262125fd500f3285bf4ba520a0d539adad
chaulb <chouladila@gmail.com>
2025-06-28 18:38:20 +0700

    [BE02] Register

--- MTS_BackEnd/MTS_BackEnd/Controllers/UserController.cs
+++ MTS_BackEnd/MTS_BackEnd/Controllers/UserController.cs
@@ -14,6 +14,24 @@ namespace MTS.BackEnd.Controllers
 			_serviceProviders = serviceProviders;
 		}
 
+		//[HttpPost("Register")]
+		//public async Task<IActionResult> Register([FromBody] LoginRequestModelDto request)
+		//{
+		//	try
+		//	{
+		//		var registerResponse = await _serviceProviders.UserService.Register(request);
+		//		if (registerResponse == null)
+		//		{
+		//			return new NotFoundObjectResult("User already exists!");
+		//		}
+		//		return new OkObjectResult(registerResponse);
+		//	}
+		//	catch (Exception ex)
+		//	{
+		//		return new BadRequestObjectResult(ex.Message);
+		//	}
+		//}
+
 		[HttpPost("Login")]
 		public async Task<IActionResult> Login([FromBody] LoginRequestModelDto request)
 		{
