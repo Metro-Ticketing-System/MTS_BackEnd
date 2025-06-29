96388516fcb2c3299eea6fe0e5689923007872d6
chaulb <chouladila@gmail.com>
2025-06-29 14:51:07 +0700

    [BE02] Register

--- MTS_BackEnd/MTS.Data/Base/BaseEntity.cs
+++ MTS_BackEnd/MTS.Data/Base/BaseEntity.cs
@@ -6,6 +6,7 @@
 		public string? CreatedBy { get; set; }
 		public string? UpdatedBy { get; set; }
 		public string? DeletedBy { get; set; }
+		public bool IsDeleted { get; set; } = false;	
 		public DateTime CreatedTime { get; set; }
 		public DateTime? LastUpdatedTime { get; set; }
 		public DateTime? DeletedTime { get; set; }
