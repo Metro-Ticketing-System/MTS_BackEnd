50e52c2f271ced041f043660eb1e5aa6a0451c81
chaulb <chouladila@gmail.com>
2025-07-14 13:49:07 +0700

    fix:base entity

--- MTS_BackEnd/MTS.Data/Base/BaseEntity.cs
+++ MTS_BackEnd/MTS.Data/Base/BaseEntity.cs
@@ -2,13 +2,19 @@
 {
 	public class BaseEntity
 	{
+		//public int Id { get; set; }
+		//public string? CreatedBy { get; set; }
+		//public string? UpdatedBy { get; set; }
+		//public string? DeletedBy { get; set; }
+		//public bool IsDeleted { get; set; } = false;	
+		//public DateTime CreatedTime { get; set; }
+		//public DateTime? LastUpdatedTime { get; set; }
+		//public DateTime? DeletedTime { get; set; }
 		public int Id { get; set; }
 		public string? CreatedBy { get; set; }
 		public string? UpdatedBy { get; set; }
-		//public string? DeletedBy { get; set; }
-		public bool IsDeleted { get; set; } = false;	
+		public bool IsDeleted { get; set; } = false;
 		public DateTime CreatedTime { get; set; }
 		public DateTime? LastUpdatedTime { get; set; }
-		//public DateTime? DeletedTime { get; set; }
 	}
 }
